305c31b78bc78d17d46b846eeb4f9ae0553866a8
Andrei-Gabriel <andrescu.gabriel.andrei@gmail.com>
2021-11-08 17:20:15 +0100

    Ticket 001

---
+++ main.js
@@ -0,0 +1,6 @@
+(document.getElementsByClassName("fas fa-bars")).addEventListener("click", 
+    function() {
+        const hamburgerMenu = document.getElementsByClassName("hamburger-menu");
+        hamburgerMenu.style.display = "initial";
+    }
+);
